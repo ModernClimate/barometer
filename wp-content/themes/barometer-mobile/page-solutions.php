08e1952586f3c379b9bf6eb46103199666c26bd2
Joe Soucheray <jsoucheray@ackmanndickenson.com>
2013-05-05 17:06:55 -0500

    solution page done

---
+++ wp-content/themes/barometer-mobile/page-solutions.php
@@ -0,0 +1,201 @@
+<?php get_header(); ?>
+
+<div class="row-fluid solutions-header center">
+	<h1 class="page-title">Change starts here.</h1>
+	<div class="solution-circle-holder">
+		<div class="solution-circle active-circle" data-page="unify">
+			<span>UNIFY</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="synchronize">
+			<span>SYNCHRONIZE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="empower">
+			<span>EMPOWER</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="anticipate">
+			<span>ANTICIPATE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="innovative">
+			<span>INNOVATE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="optimize">
+			<span>OPTIMIZE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="simplify">
+			<span>SIMPLIFY</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="allocate">
+			<span>ALLOCATE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+		<div class="solution-circle" data-page="accelerate">
+			<span>ACCELERATE</span>
+			<div class="plus-link plus-link-small">+</div>
+		</div>
+	</div>
+</div>
+
+<div class="row-fluid solutions-pages">
+	<div class="container active-pane" id="page-unify">
+		<h2>Unify<span>Your IT Portfolios.</span></h2>
+		<p>It’s common knowledge for IT executives that portfolio integration means better results. And the recommendation to “customize tactical tools designed for a specific purpose and expand their capabilities to address other portfolio requirements” is a refrain they’ve likely heard before.</p>
+		<p>The problem is that customization doesn’t solve the problem. Executives need a platform that integrates disparate silos and provides visibility across their many portfolios without costly and cumbersome workarounds. They need a platform that will give them a complete picture—one they can use to make smart decisions quickly.</p>
+		<p>barometerIT’s dynamic, multidimensional platform is shaped by member and system contributions from all IT portfolios, producing unified information in real time that can be leveraged by the entire IT value chain.</p>
+		<p>Unifying your IT portfolio allows you to:</p>
+		<p>
+			<ul>
+				<li>Generate a unified view of diverse IT portfolio assets.</li>
+				<li>Optimize IT assets, resources, and capabilities.</li>
+				<li>Manage IT-related business risk.</li>
+				<li>Synchronize your IT Enterprise.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-synchronize" style="display:none;">
+		<h2>Synchronize<span>Your IT Enterprise.</span></h2>
+		<p>Every IT leader feels the pressure to improve collaboration. Collaboration leads to increased productivity and innovation. But traditional, collaboration technologies (wikis, social networking, and the like) aren’t getting us there.</p>
+		<p>Lack of intuitive user engagement leads to more mandates and oversight and less connection and initiative. And when members do connect within these tools, it’s often not tapped in a meaningful way that can be leveraged by the team. Collaboration can only take us so far.</p>
+		<p>barometerIT moves beyond collaboration, enabling you to synchronize your IT enterprise by engaging members at the ground level. The barometerIT platform is built on member contributions, encouraging knowledge acquisition, distribution of expertise, discovery of new opportunities, and the ability to quickly respond to changing business priorities.</p>
+		<p>Synchronizing your IT enterprise allows you to:</p>
+		<p>
+			<ul>
+				<li>Improve communication and eliminate time wasted reconciling differences.</li>
+				<li>Drive innovation.</li>
+				<li>Formalize consistent capture of knowledge.</li>
+				<li>Empower your most valuable asset.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-empower" style="display:none;">
+		<h2>Empower<span>Your Most Valuable Asset.</span></h2>
+		<p>Most IT leaders will tell you that their first priority is finding the right people. High-performing team members with the right skills for the job can make or break an initiative. IT leaders know that these people likely already exist within their IT enterprise—but where?</p>
+		<p>Obligatory skills assessments, talent management, and resource management tools fail to adequately identify expertise, or worse yet, they allocate valuable resources to non-critical engagements, locking up the skills and talents most needed.</p>
+		<p>barometerIT presents a new opportunity to identify and empower your most valuable asset. Members provide ongoing, high-value contributions, visible across portfolios. In turn, they build multi-faceted relationships that stretch across company lines. barometerIT members have continually demonstrated that unification, aggressively imagined and led, drives growth and sparks new and deeper engagements.</p>
+		<p>Empowering your most valuable asset allows you to:</p>
+		<p>
+			<ul>
+				<li>Identify expertise.</li>
+				<li>Adapt to change while maintaining the flexibility to meet new challenges with the right skills, attitudes, and goals.</li>
+				<li>Shift growing levels of responsibility and decision-making down through the organization.</li>
+				<li>Identify resource impacts.</li>
+				<li>Anticipate your next problem...and solution.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-anticipate" style="display:none;">
+		<h2>Anticipate<span>Your Next Problem...and Solution.</span></h2>
+		<p>Fear of unplanned expenditures and unanticipated change can keep IT leaders up at night. The fallout from these unexpected roadblocks can result in customer disruptions, abandoned projects, and even failure to achieve desired business outcomes.</p>
+		<p>To fortify their defenses, many IT leaders are calling for more data and creating one-off dashboards in an attempt to mitigate risk and protect their portfolios. But more data doesn’t necessarily mean less risk, especially when that data remains siloed and under-utilized. The true value lies in the insights that can be gleaned when that data is unified and transformed into information.</p>
+		<p>The barometerIT platform leverages the ongoing contributions of members, as well as from siloed systems throughout your IT enterprise, to unify data. This creates dynamic, relational information that that is actionable. Increased visibility from all angles allows you to forecast changes and challenges across portfolios and proactively identify, remediate, and minimize unforeseen roadblocks and risks.</p>
+		<p>Anticipating your next problems and their solutions allows you to:</p>
+		<p>
+			<ul>
+				<li>Meet company objectives.</li>
+				<li>Deliver on time and on budget.</li>
+				<li>Mitigate risk.</li>
+				<li>Innovate new solutions.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-innovative" style="display:none;">
+		<h2>Innovative<span>New Solutions.</span></h2>
+		<p>As static IT budgets collide with the ever-increasing pressure to innovate, IT leaders can no longer afford to devote the bulk of their limited funds to supporting and maintaining existing systems.</p>
+		<p>And while most IT leaders attempt to plan accordingly in order to drive more innovation and reduce maintenance spending, innovation budgets become easy prey throughout the year to unforeseen impacts and expenses. Before long, vital funding for innovation and growth initiatives has all but disappeared.</p>
+		<p>barometerIT helps IT leaders reclaim creative, future-oriented IT work by reducing the time necessary to support their portfolios, as well as creating visibility into new opportunities for growth and streamlining the innovation process itself.</p>
+		<p>Innovating new solutions allows you to:</p>
+		<p>
+			<ul>
+				<li>Create “aha!” moments as hidden innovation opportunities are unveiled.</li>
+				<li>Focus on what matters.</li>
+				<li>Enlist everyone.</li>
+				<li>Optimize your IT value chain.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-optimize" style="display:none;">
+		<h2>Optimize<span>Your IT Value Chain.</span></h2>
+		<p>Every competitive IT leader today is striving for a leaner operation. In a quest to discover optimization opportunities, IT leaders are now looking past supply chains and striving to understand every aspect of their IT value chain, from idea to delivery.</p>
+		<p>But how we look at the IT value chain matters—adding more processes and more governance in an attempt to increase visibility only adds unnecessary bulk without generating the key insights IT leaders are looking for. Oftentimes, one set of processes is optimized without anticipating the impact these changes may have on other processes.</p>
+		<p>barometerIT generates the actionable information needed to drive decisions, empowering you to identify redundancies, streamline processes, rationalize systems, and more importantly, make decisions.</p>
+		<p>Optimizing your IT value chain allows you to:</p>
+		<p>
+			<ul>
+				<li>Remove bottlenecks.</li>
+				<li>Increase visibility.</li>
+				<li>Change rapidly.</li>
+				<li>Simplify your IT universe.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-simplify" style="display:none;">
+		<h2>Simplify<span>Your IT Universe.</span></h2>
+		<p>Over the past 30 years, the IT universe of any given company has grown increasingly complex. The usual suspects driving this complexity? Over-documentation, labor-intensive projects, and standards sprawl, not to mention a near-endless list of strategic vendors. Companies are feeling the pinch to simplify while simultaneously continuing to expand capabilities, create new opportunities, and innovate and outmaneuver increasingly nimble competitors.</p>
+		<p>In response, IT leaders are going after any low-hanging fruit that can be quickly and easily eliminated. Unfortunately, these opportunities often represent one-time expenses. IT leaders who are serious about simplifying their processes and opening up real dollars for increased innovation must dig deeper.</p>
+		<p>Using the barometerIT platform means simplifying the less tangible but more far-reaching impact of processes.</p>
+		<p>Simplifying your IT universe allows you to:</p>
+		<p>
+			<ul>
+				<li>Kick the 80/20 budget habit.</li>
+				<li>Drive innovation.</li>
+				<li>Eliminate waste.</li>
+				<li>Allocate your strategic resources.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-allocate" style="display:none;">
+		<h2>Allocate<span>Your Strategic Resources.</span></h2>
+		<p>More than ever, IT leaders are juggling a finite set of resources to achieve desired business outcomes. They shoulder the responsibility of ensuring that every available asset is focused on delivering necessary business transformation initiatives.</p>
+		<p>Unfortunately, IT leaders often find themselves working with limited visibility, forced to make rushed, reactive decisions without the information they need to strategically place their vital resources.</p>
+		<p>The barometerIT platform gives IT leaders the strategic, bird’s-eye view they need to confidently prioritize and assign resources, ensuring that the company is getting the greatest possible return on its IT investments.</p>
+		<p>Allocating your strategic resources allows you to:</p>
+		<p>
+			<ul>
+				<li>Minimize impacts.</li>
+				<li>Reduce costs.</li>
+				<li>Meet business objectives.</li>
+				<li>Accelerate your IT operations.</li>
+			</ul>
+		</p>
+	</div>
+	<div class="container" id="page-accelerate" style="display:none;">
+		<h2>Accelerate<span>Your IT Operation.</span></h2>
+		<p>Speed and agility are more important to the success of business now than ever before. The rapid-fire pace of new consumer IT innovation has raised the technology expectations of companies and customers. They demand to get new features and functionality at the same pace.</p>
+		<p>Successful IT organizations of the future must find a way to continuously and rapidly change how they support their portfolios as well as innovate. IT organizations will continue to impact the speed of change if they can’t adapt to the fast pace nature of business and innovation.</p>
+		<p>Organizations using the barometerIT platform see accelerated results across IT portfolios: faster product development, reduced costs, and solution delivery.</p>
+		<p>Accelerating your IT operation allows you to:</p>
+		<p>
+			<ul>
+				<li>Reduce workflow interruption.</li>
+				<li>Increase customer service.</li>
+				<li>Respond to change.</li>
+				<li>Meet business objectives.</li>
+			</ul>
+		</p>
+	</div>
+</div>
+
+<script type="text/javascript">
+	$(function () {
+		$('.solution-circle, .plus-link').on('touchEnd click', function () {
+			$('.active-circle').removeClass('active-circle');
+			if ($(this).parent().hasClass('solution-circle')) {
+				$(this).parent().addClass('active-circle');
+			} else {
+				$(this).addClass('active-circle');
+			}
+			var pane = $(this).attr('data-page');
+			$('.active-pane').removeClass('active-pane').fadeOut(400, function () {
+				$('#page-'+pane).addClass('active-pane').fadeIn();
+			});
+		});
+	});
+</script>
+
+<?php get_footer(); ?>
