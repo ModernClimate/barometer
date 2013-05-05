6a51e6978502887aa375faad1ee65992fac3b1ce
Joe Soucheray <jsoucheray@ackmanndickenson.com>
2013-05-05 17:59:53 -0500

    about-us page

---
+++ wp-content/themes/barometer-mobile/page-about-us.php
@@ -0,0 +1,118 @@
+<?php get_header(); ?>
+
+<div class="about-us">
+
+<h1>People Driven IT.<font class='smaller'>&#0153;</font></h1>
+
+<div class="container">
+	<div class="row-fluid">
+
+		<div class="span3">
+			<div class="person">
+				<div class="person_info">PAUL<br />CRAMER</div>
+				<div class="contact_info">Founder</div>
+		        <a class="envelope" href="mailto:&#112;&#097;&#117;&#108;&#046;&#099;&#114;&#097;&#109;&#101;&#114;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+		        	<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+		        </a>
+		    </div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+				<div class="person_info">TAMER<br />SELIM</div>
+				<div class="contact_info">VP Product Strategy<br />&amp; Marketing</div>
+				<a class="envelope" href="mailto:&#116;&#097;&#109;&#101;&#114;&#046;&#115;&#101;&#108;&#105;&#109;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+					<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+				</a>
+			</div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+				<div class="person_info">TIM<br />WELLS</div>
+				<div class="contact_info">EVP Sales</div>
+				<a class="envelope" href="mailto:&#116;&#105;&#109;&#046;&#119;&#101;&#108;&#108;&#115;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+					<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+				</a>
+			</div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+				<div class="person_info">JAMIE<br />FRAGOLA</div>
+				<div class="contact_info">VP Business Development</div>
+				<a class="envelope" href="mailto:&#106;&#097;&#109;&#105;&#101;&#046;&#102;&#114;&#097;&#103;&#111;&#108;&#097;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+					<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+				</a>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+<div class="container">
+	<div class="row-fluid">
+
+		<div class="span3">
+			<div class="person">
+	            <div class="person_info">AARON<br />GJERDE</div>
+	            <div class="contact_info">VP Client Engagement</div>
+	            <a class="envelope" href="mailto:&#097;&#097;&#114;&#111;&#110;&#046;&#103;&#106;&#101;&#114;&#100;&#101;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+	            	<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+	            </a>
+	        </div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+	            <div class="person_info">PAT<br />JOSEPHSON</div>
+	            <div class="contact_info">VP Product Development</div>
+	            <a class="envelope" href="mailto:&#112;&#097;&#116;&#046;&#106;&#111;&#115;&#101;&#112;&#104;&#115;&#111;&#110;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+	            	<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+	            </a>
+	        </div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+	            <div class="person_info">GERRY<br />BLANCK</div>
+	            <div class="contact_info">VP Engineering</div>
+	            <a class="envelope" href="mailto:&#103;&#101;&#114;&#114;&#121;&#046;&#098;&#108;&#097;&#110;&#099;&#107;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+	            	<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+	            </a>
+	        </div>
+		</div>
+
+		<div class="span3">
+			<div class="person">
+	            <div class="person_info">BRENT<br />WORDEN</div>
+	            <div class="contact_info">VP Research<br />&amp; Development</div>
+	            <a class="envelope" href="mailto:&#098;&#114;&#101;&#110;&#116;&#046;&#119;&#111;&#114;&#100;&#101;&#110;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">
+	            	<img src="<?php bloginfo('template_directory'); ?>/css/imgs/small-envelope.png" />
+	            </a>
+	        </div>
+		</div>
+
+	</div>
+</div>
+
+
+
+<div class="container">
+	<div class="row-fluid about-content center">
+		<p>The barometerIT team is made up of the people who drive IT. In the past, we’ve each played multiple roles to deliver successful IT projects &mdash; enterprise architects, technical leads, developers, project and program managers, business leaders &mdash; and we each felt that something was missing. We wanted a tool that would enable us to work collaboratively, with less waste and more results.</p>
+	    <h2>We wanted Unified IT in real time.</h2>
+	    <p>And so barometerIT was born &mdash; a platform built for IT + Business, by IT + Business, designed to help us get where we’re going, faster, leaner, together.</p>
+	    <h2>If you enjoy hanging out at the intersection of IT and business, <a href="#" class="scroll_to_careers" data_scroll=".careers_container">join us</a>.</h2>
+	</div>
+</div>
+
+<br />
+<br />
+<br />
+
+</div>
+
+
+
+<?php get_footer(); ?>
