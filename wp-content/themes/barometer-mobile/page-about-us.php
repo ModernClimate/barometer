0f6c5e2dea9d0cdf54c01fbb4645489270b48ced
Joe Soucheray <jsoucheray@ackmanndickenson.com>
2013-05-05 20:34:06 -0500

    added in careers

--- wp-content/themes/barometer-mobile/page-about-us.php
+++ wp-content/themes/barometer-mobile/page-about-us.php
@@ -103,7 +103,7 @@
 		<p>The barometerIT team is made up of the people who drive IT. In the past, we’ve each played multiple roles to deliver successful IT projects &mdash; enterprise architects, technical leads, developers, project and program managers, business leaders &mdash; and we each felt that something was missing. We wanted a tool that would enable us to work collaboratively, with less waste and more results.</p>
 	    <h2>We wanted Unified IT in real time.</h2>
 	    <p>And so barometerIT was born &mdash; a platform built for IT + Business, by IT + Business, designed to help us get where we’re going, faster, leaner, together.</p>
-	    <h2>If you enjoy hanging out at the intersection of IT and business, <a href="#" class="scroll_to_careers" data_scroll=".careers_container">join us</a>.</h2>
+	    <h2>If you enjoy hanging out at the intersection of IT and business, <a href="#" id="join-us">join us</a>.</h2>
 	</div>
 </div>
 
@@ -113,6 +113,120 @@
 
 </div>
 
+<div class="row-fluid" style="background:#235088;display:none;" id="jobs">
+	<div class="container">
+		<br />
+		<h1>Careers</h1>
+		<br />
+		<div class="tabbable tabs-left">
+		<ul class="nav nav-tabs tabs-left" id="jobs-tabs">
+			<li class="active"><a href="#java">Java Engineer</a></li>
+			<li><a href="#ui">UI Engineer</a></li>
+		</ul>
+		<div class="tab-content">
+		  <div class="tab-pane active" id="java">
+		  	<h3>Java Engineer</h3>
+	        <p>
+	        	Barometer is a fast growing Minneapolis software startup built from the ground up by developers. We are pioneering a new market segment, Unified IT, and our product is shaping and defining this segment on a daily basis. Barometer is growing fast and we are looking to build our team of rock star developers.
+	        </p>
+	        <p>
+	        	We have top-notch development talent, experienced management, cool technology, and paying customers. All of this makes Barometer a great place to work, but we did not stop there. We pride ourselves on having built one of the most developer-friendly work environments in the Twin Cities. Here are a few highlights of Barometer’s culture from the previous year:
+	        </p>
+	          <ul>
+	            <li>Free lunches during the last week of each development iteration</li>
+	            <li>Impromptu trips to the movie theater
+	            <li>Free pop, snacks, and beer stocked in our kitchen
+	            <li>Team happy hours at a variety of tap rooms within walking distance of the office
+	            <li>Weekly high score contests on our office arcade game
+	            <li>Sonos pumping out the tunes through our office audio system
+	          </ul>
+	        <p>
+	        	We have immediate openings for smart developers who thrive in a dynamic, fast-paced environment and are passionate about delivering software on time, with quality, and no surprises. Do you have an entrepreneurial spirit, want to participate in a culture of leadership, take pride in and own your daily activities? If so, and your background covers the requirements listed below, apply now by emailing <a href="mailto:&#099;&#097;&#114;&#101;&#101;&#114;&#115;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">careers at barometerit.com</a>.
+	        </p>
+
+	        <h4>Responsibilities</h4>
+	        <ul>
+	          <li>Understand, adhere to, and contribute to standards and best practices</li>
+	          <li>Implement technical solutions that satisfy product requirements</li>
+	          <li>Ensure quality of delivered solutions using automated testing tools</li>
+	          <li>Ensure delivered solutions adhere to company best practices and standards</li>
+	          <li>Make certain delivered solutions have optimum performance in a highly scalable infrastructure</li>
+	          <li>Deliver solutions following development patterns and reference application architecture</li>
+	          <li>Own the delivery of features and tasks based on provided estimates</li>
+	          <li>Participate in and contribute to our culture of leadership</li>
+	          <li>Contribute ideas for continually improving team productivity, job satisfaction, and code quality</li>
+	          <li>Resolve defects in new and existing features</li>
+	          <li>Collaborate with, mentor, and be mentored by peers and coworkers</li>
+	          <li>Provide accurate and detailed status to team members and management</li>
+	          <li>Have fun engineering software</li>
+	        </ul>
+	        <h4>Experience</h4>
+	        <ul>
+	          <li>3+ years in information technology</li>
+	          <li>2+ years specializing in the Java and JEE platform</li>
+	          <li>Participation in all phases of the software development life cycle</li>
+	          <li>Experience in one or more development methodologies</li>
+	        </ul>
+	        <h4>Skills</h4>
+	        <ul>
+	          <li>Communicates effectively with fellow engineers and product managers via Verbal, Written, Byte code, Email, Instant Messenger</li>
+	          <li>Works independently with minimal oversight</li>
+	          <li>Knowledge of design patterns:</li>
+	          <li>Singleton, Factory, Feng Shui, Command</li>
+	          <li>Working knowledge of the majority of our technology stack which consists of:</li>
+	          <li>Spring, Hibernate, Solr, Lucene, Infinispan, JBoss Application Server, Mule, ActiveMQ, Zookeeper, Minecraft, MySQL, Maven, JUnit</li>
+	        </ul>
+	        <h4>Education</h4>
+	        <ul>
+	          <li>Bachelors Degree or higher in Computer Science or related field.</li>
+	        </ul>
+		  </div>
+		  <div class="tab-pane" id="ui">
+		  	<h3>UI Engineer</h3>
+	        <p>
+	          At Barometer, we have top-notch development talent, experienced management, and Fortune 500 customers. We tackle tough problems and use interesting technology in the process. We are looking for a Front-End Web Developer to build on our highly interactive, information-rich web UI as we forge ahead into the uncharted blue waters of the Unified IT market we are helping to define. Apply now by emailing <a href="mailto:&#099;&#097;&#114;&#101;&#101;&#114;&#115;&#064;&#098;&#097;&#114;&#111;&#109;&#101;&#116;&#101;&#114;&#105;&#116;&#046;&#099;&#111;&#109;">careers at barometerit.com</a>
+	        </p>
+	        <p>
+	          Our office is located in NE Mpls. It’s central to the metro area and we have some nice office perks (food, video games, music, beer etc) to keep us happy. If working to build a large-scale web application in a fast-paced and dynamic environment is something that interests you, please check us out.
+	        </p>
+	        <h4>Responsibilities</h4>
+	        <ul>
+	        	<li>Implement front-end features that tackle complex product requirements, are bullet-proof cross-browser, and delight our customers.</li>
+	        	<li>Ensure quality using automated testing tools, and by following best practices and reference architectures.</li>
+	        	<li>Break down high-level features into actionable, estimate-able tasks</li>
+	        	<li>Resolve defects in new and existing features</li>
+	        	<li>Communicate clearly and frequently. The development team meets briefly every morning to stay coordinated, and collaborates to solve hard problems on a regular basis.</li>
+	        	<li>Contribute ideas for continually improving customer experience, team productivity, job satisfaction, and code quality</li>
+	        </ul>
+	        <h4>Experience</h4>
+	        <ul>
+	        	<li>3+ years building web app front-ends, including HTML/CSS and plenty of Javascript. An intimate understanding of browser behavior, including maneuvering cross-browser constraints and employing progressive enhancement whenever possible is a must. Portfolio of work and code samples are desirable, but not required to apply.</li>
+	        	<li>Experience with client-side MV* frameworks, CSS pre-processors, UX design, and data visualization are big plusses.</li>
+	        	<li>General end-to-end experience in modern web application frameworks, and/or with RESTful backend API's are also a plus.</li>
+	        	<li>Proficiency in Java (and the Wicket framework, in particular) is a plus.</li>
+	        	<li>Having worked with a team of developers on a large-scale web application is a plus. This includes working in an IDE, using source control, and using an integrated build process.</li>
+	        </ul>
+		  </div>
+		</div>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	$(function () {
+		$('#jobs-tabs a').click(function (e) {
+		  e.preventDefault();
+		  $(this).tab('show');
+		});
+		$('#join-us').click(function (evt) {
+			evt.preventDefault();
+			$('#jobs').show();
+			$.scrollTo($('#jobs'), {
+				'offset': -50
+			});
+		});
+	});
+</script>
 
 
 <?php get_footer(); ?>
