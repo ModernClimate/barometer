32f209affa11fc733b102f68d387559d65dcaed8
Joe Soucheray <jsoucheray@ackmanndickenson.com>
2013-05-05 21:17:37 -0500

    responsive fixes for tablet

--- wp-content/themes/barometer-mobile/footer.php
+++ wp-content/themes/barometer-mobile/footer.php
@@ -1,6 +1,11 @@
 <div class="footer">
-	<?php wp_nav_menu( array('menu' => 'footer', 'menu_class' => 'nav')); ?>
+	<?php wp_nav_menu( array('menu' => 'footer', 'menu_class' => 'nav pull-right')); ?>
 </div>
+<script type="text/javascript">
+	$(function () {
+		$('.footer li a').append('<span class="plus-link">+</span>');
+	});
+</script>
 <?php wp_footer(); ?>
 </body>
 </html>
